2ff402ef959cbaa7a4251eb17435540598292357
girnal82 <grinal82@gmail.com>
2023-01-21 20:30:24 +0300

    fixed speed of click counter

--- cookie-clicker/task.js
+++ cookie-clicker/task.js
@@ -1,9 +1,16 @@
 const img = document.getElementById("cookie");
+let clickTimer = document.getElementById("click__timer");
+let start = 0;
+let end;
 
-let lastClick = new Date();
 
 function cookieClicker() {
-    let currentTime = new Date();
+    end = new Date();
+    if (start !== 0) {
+        let currentSpeed = 1000 / (end - start);
+        clickTimer.textContent = currentSpeed.toFixed(2).toString();
+    }
+
     document.getElementById("clicker__counter").textContent =
         Number(document.getElementById("clicker__counter").textContent) + 1;
     if (document.getElementById("clicker__counter").textContent % 2 != 0) {
@@ -16,9 +23,8 @@ function cookieClicker() {
         img.height -= 20;
         img.width -= 20;
     }
-    result = (currentTime - lastClick) / 1000;
-    document.getElementById("click__timer").textContent = result;
-    lastClick = currentTime;
+    start = new Date();
+
 }
 
 img.onclick = cookieClicker;
