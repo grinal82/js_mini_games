1649bf3a19e5edf635b1eeaa82b0ebdc3b38dc8d
girnal82 <grinal82@gmail.com>
2023-01-21 19:57:37 +0300

    добавил однуление счетчика

--- countdown/task.js
+++ countdown/task.js
@@ -2,7 +2,8 @@ const myTimer = document.getElementById("timer");
 
 setInterval(() => {
     myTimer.textContent = Number(myTimer.textContent) - 1;
-    if (myTimer.textContent == 0) {
+    if (myTimer.textContent == -1) {
         alert("Вы победили в конкурсе!");
+        myTimer.textContent = 11;
     }
 }, 1000);
